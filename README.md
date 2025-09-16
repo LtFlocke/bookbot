9dbafe3b13fa3cee80d38f03641c7d2abf90a24c
LtFlocke <marv.ernst2000@gmail.com>
2025-09-17 00:24:54 +0200

    Added searching capability and updated readme

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ BookBot is my first [Boot.dev](https://www.boot.dev) project!
 
 BookBot can evaluate data of a provided .txt file. It returns the total number of characters as well as an ordered list of the most common alphabetic characters.
 
-Usage: python3 main.py <path to .txt file>
+Usage: python3 main.py
+
+The Program will then ask to provide the name of the Book. It can analyze any book stored at /books/
 
 Created my Marvin | Flocke through boot.dev course.
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import sys
+import os.path
 
-if len(sys.argv) != 2:
-	print("Usage: python3 main.py <path_to_book>")
-	sys.exit(1)
-
-def main(filepath=sys.argv[1]):
+def main():
 	from stats import count_words
 	from stats import count_characters
 	from stats import sort_dictionaries
-	filepath = filepath
+	print("Welcome to BookBot!")
+	print("Please enter the name of the book file to analyze:")
+	book_name = input()
+	print(f"Trying to find Book: {book_name}")
+	book_name = book_name.strip()
+	book_name = book_name.lower()
+	filepath ="books/" + book_name.replace(" ","") +".txt"
+	if os.path.isfile(filepath) == False:
+		print(f"Error: Could not find file {filepath}")
+		sys.exit()
 	char_count = count_characters(filepath)
 	num_words = count_words(filepath)
 	char_sort = sort_dictionaries(filepath)
